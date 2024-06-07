0fc070bd9a33f1596cdc661984ccacd061ba6f4b
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2024-06-07 09:53:50 +0530

    updates

--- first-project/src/components/30-03/Navbar2.jsx
+++ first-project/src/components/30-03/Navbar2.jsx
@@ -26,6 +26,7 @@ const Navbar2 = ({search, handleChange}) => {
         <h2>Search Product:</h2>
         <input placeholder="Mens.." value={search} onChange={handleChange} />
         {state?.user?.name && <h1>Hello! {state?.user?.name}</h1>}
+        
         <button style={{width: "130px", height: "50px", fontSize: "2px", textAlign: "center"}}>{state?.user?.role ? <h1 onClick={Logout}>Logout</h1> : <h2 onClick={() => router("/login")}>Login</h2>}</button>
         {/* <button onClick={() => router("/profile")}>Login</button> */}
       </div>
