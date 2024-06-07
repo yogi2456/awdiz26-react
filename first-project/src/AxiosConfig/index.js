c9183ab74e64615a9dd604087ead5720af7101fd
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2024-06-07 11:46:38 +0530

    updates

--- first-project/src/AxiosConfig/index.js
+++ first-project/src/AxiosConfig/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "https://backend-7grf.onrender.com/",
+    baseURL: "https://backend-7grf.onrender.com",
     // baseURL: process.env.ENV === "DEVELOPMENT" ? "http://localhost:3003/api/v1" : "https://awdiz-6.com",
     withCredentials: true,
 });
